33c7f2bebc94389e81f9d7ff27d48b5a269e36f5
Kalle Siukola <kallesiukola@Mac.wlan>
2025-08-31 18:33:01 +0300

    Something else

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ app.get("/", (req: Request, res: Response) => {
   res.send("hello world");
 });
 
-// Healthcheck endpoint -> returns 200
+// Healthcheck endpoint -> returns 200ggg
 app.get("/healthcheck", (req: Request, res: Response) => {
   res.sendStatus(200);
 });
